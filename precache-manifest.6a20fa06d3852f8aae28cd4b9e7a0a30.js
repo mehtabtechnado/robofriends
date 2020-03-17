df38902d2bff6552174bf78d812afd96a8bfc28b
Mehtab <mehtab.farooq@technado.ltd>
2020-03-17 11:56:20 +0500

    Updates

---
+++ precache-manifest.6a20fa06d3852f8aae28cd4b9e7a0a30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1a4856454fd6d6c300842babcae980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "f41341f7fa9db58e92eb",
+    "url": "/static/css/main.6b8501d4.chunk.css"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/static/js/2.3a7757fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3a7757fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41341f7fa9db58e92eb",
+    "url": "/static/js/main.964a0bbd.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
